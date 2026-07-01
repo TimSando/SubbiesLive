379e24eadf4c2b788366a2ca641f87cb340b9e4e
Tim Sanderson <timothy.sanderson93@gmail.com>
2026-07-02 09:52:42 +1000

    fixed formatting on the comp page

--- frontend/src/pages/Competitions.jsx
+++ frontend/src/pages/Competitions.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useApi } from '../hooks/useApi.js'
 import { api } from '../api/client.js'
+import PageSubscribeButton from '../components/NotificationToggle/PageSubscribeButton.jsx'
 
 export default function Competitions() {
   const { data: competitions, loading, error } = useApi(() => api.getCompetitions(), [])
@@ -250,12 +251,7 @@ export default function Competitions() {
                         {sortGrades(comps).map(comp => (
                           <div key={comp.id} className="comp-row">
                             <div className="comp-row__info">
-                              <div className="comp-row__logo-placeholder">
-                                <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" style={{ opacity: 0.8 }}>
-                                  <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-                                  <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-                                </svg>
-                              </div>
+                              <PageSubscribeButton topicType="competition" topicId={comp.id} topicName={comp.name} />
                               <div className="comp-row__meta">
                                 <div className="comp-row__name">{comp.name}</div>
                               </div>
